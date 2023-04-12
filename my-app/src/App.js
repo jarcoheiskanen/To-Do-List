df7d862bd676b0d86bf6bd5dbfec96811cd21721
jarcoheiskanen <jarco.heiskanen@gmail.com>
2023-04-12 11:17:47 +0300

    Start

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+
+// -- Default imports
+
+import logo from './logo.svg';
+import './App.css';
+
+// -- Pages
+
+import MainPage from './pages/mainPage.js'
+
+// -- Functions
+
+function App() {
+  return (
+    <div className="App">
+
+      <MainPage></MainPage>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/pages/mainPage.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+// -- Imports
+
+import React, { useState } from 'react';
+import PageStyle from '../style/mainPage.css'
+
+// -- Function
+
+const Page = () => {
+
+    return (
+        <div className="backgroundTest">
+            <h1>Moi</h1>
+        </div>
+    );
+};
+
+export default Page;
